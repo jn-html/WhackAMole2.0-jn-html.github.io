e2d049c7822e13e293ec348bbb6f6f4c500b12f7
jn-html <marmainjena @gmail.com>
2021-05-14 15:35:37 +0200

    Refactor code

--- index.js
+++ index.js
@@ -2,32 +2,28 @@ const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
 
-
 let gameSpeed = document.getElementById("game-speed");
 // // Alternatively if your select resides inside a form, you could use this:
 // document.getElementById("myForm").elements["a_drop_down_box"];
 
-// Get the value of each option in the select list
 let value = gameSpeed.options[gameSpeed.selectedIndex].value;
 
 // To prevent having a mole pop up twice at the same place in a row
 let lastHole;
-
 let timeUp = false;
-let timer = 15000;
 let score = 0 ;
-let counter = timer / 1000;
 
-// console.log(counter);
+// ### RANDOMTIME (start) ###
 
-document.getElementById('counter').innerHTML = counter;
-
-
-// random amount of time for a mole to pop up
+// random amount of time
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
+// ### RANDOMTIME (end) ###
+
+
+// ### RANDOM HOLE POP (start) ####################
 
 // pick a random hole for the mole to pop up
 function randomHole(holes) {
@@ -35,7 +31,7 @@ function randomHole(holes) {
   const hole = holes[idx];
   // to prevent having twice the same hole, if it's the same we run randomHole again
   if (hole === lastHole) {
-    // console.log('Ah nah that\'s the same one bud');
+    console.log('Ah nah that\'s the same one bud');
     return randomHole(holes);
   }
   // console.log(holes.length);
@@ -44,6 +40,10 @@ function randomHole(holes) {
   return hole;
 }
 
+// ### RANDOM HOLE POP (end) ####################
+
+
+// ### MOLE POP (start) #############
 
 // make the mole pop up the selected hole
 function pop() {
@@ -55,7 +55,6 @@ function pop() {
   const normalSpeed = randomTime(300, 800);
   const ninjaSpeed = randomTime(200, 400);
   
-  // modify speed of mole pop up depending on select
   if (gameSpeed.value === 'ninjaSpeed') {
     time = ninjaSpeed
   } else if (gameSpeed.value === 'grandpaSpeed') {
@@ -63,7 +62,7 @@ function pop() {
   } else {
     time = normalSpeed
   }
-  
+    
   const hole = randomHole(holes);
   // console.log(time, hole);
   hole.classList.add('up');
@@ -75,54 +74,27 @@ function pop() {
   }, time);
 }
 
-function countDownFinish() {
-  clearInterval(gameCounter);
-  document.getElementById("counter").innerHTML = counter;	
-}
+// ### MOLE POP (end) #############
 
-function countDown() {
-  gameCounter = counter
-  
-  setInterval(() => {
-    document.getElementById('counter').innerHTML = gameCounter;
+// ###############################
+// ### ONCE GAME START ########## 
+// ###############################
 
-    if (gameCounter === 0) countDownFinish();
-    else {
-      gameCounter--;
-      // console.log(gameCounter);
-      // console.log(counter);
-    }    
-  }, 1000); 
-  
-}
 
-function gameOver() {
-  // let timer = 15000;
-  // timeUp = false;
-  counter = timer / 1000;
-  
-  setTimeout(()=> {
-    timeUp = true;
-    console.log('Game Over');
-  }, timer)  
-}
+// ### GAME START (start) ########
 
-function startGame() {  
-  timeUp = false;
+function startGame() {
   scoreBoard.textContent = 0;
+  timeUp = false;
   score = 0;
-  countDown()
   pop();
-  gameOver();
-
   // set in game timer, not visible yet
-  // setTimeout(()=> {
-  //   timeUp = true;
-  //   console.log('Game Over');
-  // }, timer)
-
+  setTimeout(()=> timeUp = true, 10000)
 }
+// ### GAME START (end) ########
 
+
+// ### MOLE HIT (start) #####
 function bonk(e) {
   if(!e.isTrusted) return; // cheaters
   score++;
@@ -130,10 +102,8 @@ function bonk(e) {
   scoreBoard.textContent = score;
 }
 
-// Setting up a timer for later use
-
-
 moles.forEach(mole => mole.addEventListener('click', bonk));
-
 // see below for better mobile UX
 // moles.forEach(mole => mole.addEventListener('', bonk));
+
+// ### MOLE HIT (end) ####
